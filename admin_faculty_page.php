781f30179068c7acae476e57925746d503227b9c
HarsH <bokharsh@gmail.com>
2019-10-16 19:58:06 +0530

    fixed delete temporarly need to add confirmation error in adding faculty modal not displaying

--- admin_faculty_page.php
+++ admin_faculty_page.php
@@ -7,13 +7,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
 
       <script>
       $(document).ready(function(){
         $("#mytable #checkall").click(function () {
                 if ($("#mytable #checkall").is(":checked")) {
-                    $("#mytable input[type=checkbox]").each(function () {
+                    $("#mytable input[type=checkbox]").each(function
+                       () {
                         $(this).prop("checked", true);
                     });
 
@@ -33,145 +35,183 @@
 
 <body>
 
+  <!-- php code for connecting to the database -->
   <?php
      $server = "HARSH";
      $conn = sqlsrv_connect( $server, array( 'Database' => 'KNITCSE' ) );
-
      $stmt = sqlsrv_query( $conn, "select * from Admin_teachers_table",array()); //making query and storing it in stmt variable
-
-
-
-
-  //echo starts for displaying the top of the page
+  // for displaying the top of the page
   ?>
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-  <!------ Include the above in your HEAD tag ---------->
-
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
 
-<?php
-  if ($stmt !== NULL) {
-    //echo "printing the table\n";
-?>
-    <div class="container">
-  	<div class="row">
+  <?php
+  //checking the result of fetching the contents
+    if ($stmt !== NULL) {
+      //echo "printing the table\n";
+  ?>
+  <div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h4>List Of Faculty</h4>
+      <div class="table-responsive">
+        <table id="mytable" class="table table-bordred table-striped">
+          <thead>
+            <th><input type="checkbox" id="checkall" /></th>
+            <th>Name</th>
+            <th>Employee No</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </thead>
+          <tbody>
+          <?php
+          while($rows = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
+          ?>
+            <tr>
+            <td><input type="checkbox" class="checkthis" /></td>
+            <td><?php echo $rows["Name"] ?></td>
+            <td><?php echo $rows["EmpNo"]?></td>
+            <!-- delete and edit -->
+            <td><p data-placement="top" data-toggle="tooltip" title="Edit">
+              <button class="btn btn-primary btn-xs"  >
+                <span class="glyphicon glyphicon-pencil"></span>
+              </button>
+            </p></td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Delete">
+              <form action="delete_on_eno.php" method="post">
+               <button class="btn btn-danger btn-xs"  name="submit" value="<?php echo $rows['EmpNo']?>" >
+                <span class="glyphicon glyphicon-trash"></span>
+              </button>
+            </form>
+            </p></td>
+            </tr>
+          <?php
+          }
+          }
+          ?>
+          </tbody>
+        </table>
 
+        <!-- page number and next page code -->
+        <div class="clearfix"></div>
+        <ul class="pagination pull-right">
+          <li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></li>
+          <li class="active"><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">4</a></li>
+          <li><a href="#">5</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  </div>
 
-          <div class="col-md-12">
-          <h4>List Of Faculty</h4>
-          <div class="table-responsive">
+  <!-- delete function if want to do it through js -->
+  <script>
+    function delete_faculty(eid){
 
+    }
+  </script>
 
-                <table id="mytable" class="table table-bordred table-striped">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#add_faculty">Add Faculty</button>
 
-                     <thead>
+        <!-- modal -->
 
-                     <th><input type="checkbox" id="checkall" /></th>
-                     <th>Name</th>
-                      <th>Employee No</th>
-                        <th>Edit</th>
-                         <th>Delete</th>
-                     </thead>
+        <div class="modal" id="add_faculty" >
+        <div class="modal-dialog">
+          <div class="modal-content">
 
-    <tbody>
-<?php
-while($rows = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
-
-?>
-
-
-    <tr>
-    <td><input type="checkbox" class="checkthis" /></td>
-    <td><?php echo $rows["Name"] ?></td>
-    <td><?php echo $rows["EmpNo"]?></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-    </tr>
-<?php
-}
-}
-?>
-    </tbody>
-
-</table>
-<div class="clearfix"></div>
-<ul class="pagination pull-right">
-  <li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></li>
-  <li class="active"><a href="#">1</a></li>
-  <li><a href="#">2</a></li>
-  <li><a href="#">3</a></li>
-  <li><a href="#">4</a></li>
-  <li><a href="#">5</a></li>
-  <li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
-</ul>
-
+            <!-- Modal Header -->
+            <div class="modal-header">
+              <h4 class="modal-title">Modal Heading</h4>
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
 
-        </div>
-	</div>
+            <!-- Modal body -->
+            <div class="modal-body">
+              Modal body..
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
+          </div>
+          </div>
+          </div>
+
+</div>
+</div>
 </div>
 
 
-<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-      <div class="modal-dialog">
+  <!-- was for edit button alert working -->
+
+  <!-- <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+    <div class="modal-dialog">
     <div class="modal-content">
-          <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+          <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+        </button>
         <h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
       </div>
-          <div class="modal-body">
-          <div class="form-group">
-        <input class="form-control " type="text" placeholder="Mohsin">
+      <div class="modal-body">
+        <div class="form-group">
+          <input class="form-control " type="text" placeholder="Mohsin">
         </div>
         <div class="form-group">
-
-        <input class="form-control " type="text" placeholder="Irshad">
+          <input class="form-control " type="text" placeholder="Irshad">
         </div>
         <div class="form-group">
-        <textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
-
-
+          <textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
         </div>
       </div>
-          <div class="modal-footer ">
+      <div class="modal-footer ">
         <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
       </div>
-        </div>
-    <!-- /.modal-content -->
-  </div>
-      <!-- /.modal-dialog -->
     </div>
 
+    </div>
+
+  </div> -->
 
 
-    <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-      <div class="modal-dialog">
+  <!-- was for delete button alert working -->
+
+  <!-- <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+    <div class="modal-dialog">
     <div class="modal-content">
-          <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+          <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+        </button>
         <h4 class="modal-title custom_align" id="Heading">Delete this entry</h4>
       </div>
-          <div class="modal-body">
-
+      <div class="modal-body">
        <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this Record?</div>
-
       </div>
-        <div class="modal-footer ">
+      <div class="modal-footer ">
         <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
         <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
       </div>
-        </div>
-    <!-- /.modal-content -->
-  </div>
-      <!-- /.modal-dialog -->
     </div>
+    </div>
+  </div> -->
 
-<?php sqlsrv_free_stmt($stmt);
-sqlsrv_close($conn);
-?>
+  <!-- eding the php script and closing the connection -->
+  <?php sqlsrv_free_stmt($stmt);
+  sqlsrv_close($conn);
+  ?>
 
 </body>
 </html>
