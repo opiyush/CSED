a60226fc3a5c1f2bb5584e97d015e375a1a24a38
HarsH <bokharsh@gmail.com>
2019-10-18 22:32:12 +0530

    fixed except nav bar and edit option

--- admin_assignment_page.php
+++ admin_assignment_page.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Admin->Assignments </title>
+    <link rel="stylesheet" href="Login.css">
     <!-- <link rel="stylesheet" href=""> -->
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -16,7 +17,7 @@
   </head>
 
 <body>
-  <?php //include 'header.php' ?>
+  <?php include 'header.php' ?>
 
   <!-- php code for connecting to the database -->
   <?php
--- admin_staff_page.php
+++ admin_staff_page.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Admin->staff </title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="Login.css">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -39,7 +39,7 @@
   </head>
 
 <body>
-  <!-- <?php include 'header.php' ?> -->
+  <?php include 'header.php' ?>
   <!-- php code for connecting to the database -->
   <?php
      $server = "HARSH";
