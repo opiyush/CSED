c0c8076f7230f611eed932166b09dc59cccc5399
Nikhil Pandey <vinayak.pp73@gmail.com>
2019-10-19 23:28:08 +0530

    Academic

--- header.php
+++ header.php
@@ -1,84 +1,82 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="Login.css">
-</head>
-<body>
-<?php include 'header.php';?>
-<?php include 'connection.php' ;?>
 
-<?php
-$stmt = sqlsrv_query( $conn, "select Subject, Semester, Sub_Code from Subjects_table order by Semester",array());
-//echo starts for displaying the top of the page
-?>
-  <div id="fir" class="container">
-    <div class="row mt-5">
+<style>
 
-      <?php
-for ($x = 1; $x <=4; $x++) {
-    ?>
-      <div class="col-md-3  mb-5">
-        <div class="card" >
-          <div class="card-body">
-            <h5 class="card-title">SEMESTER <?php echo $x ?> </h5>
-            <div class="dropdown">
-              <form action="Assignment.php" method="post">
-                <label for="SUBJECT">SUBJECT</label>
-                <select name="subject" class="form-control">
-                  <?php
-                  while($rows = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) and $rows["Semester"] == $x){
-                    ?>
-                  <option value="<?php echo $rows["Sub_Code"]?>"> <?php echo $rows["Subject"]?></option>
-                  <<?php } ?>
-                </select>
-                <button type="submit" class="btn">FIND</button>
-              </form>
-            </div>
-          </div>
-        </div>
+
+</style>
+  <div class="header">
+    <div class="row">
+      <div class="col-md-1">
+        <a href="" class="logo"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/KNITlogo.png/220px-KNITlogo.png" alt="KNIT Logo" id="knitlogo"></a>
       </div>
-      <?php
-      }
-      ?>
-
-      <?php
-      for ($x = 5; $x <=8; $x++) {
-      ?>
-      <div class=" col-md-3">
-        <div class="card" >
-          <div class="card-body">
-            <h5 class="card-title">SEMESTER <?php echo $x ?></h5>
-            <div class="dropdown">
-              <form action="Assignment.php" method="post">
-                <label for="SUBJECT">SUBJECT</label>
-                <select name="subject" class="form-control">
-                  <?php
-                  while($rows = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) and  $rows["Semester"]==$x){
-                    ?>
-                  <option value="<?php $rows["Sub_Code"]?>"><?php echo $rows["Subject"]?></option>
-                  <<?php } ?>
-                </select>
-                <button type="submit" class="btn">FIND</button>
-              </form>
-            </div>
-          </div>
-        </div>
+      <div class="col-md-11">
+        <h1>Computer Science And Engineering</h1>
+        <h2>KNIT Sultanpur</h2>
       </div>
-  <?php
-}
-?>
-
     </div>
-</div>
+    <hr>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="Front_Page.php">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">About <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="Faculty.php">Faculty <span class="sr-only">(current)</span></a>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Academic Corner
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="Assignment1.php"><span class="sr-only">(current)</span>Assignment</a>
+            <a class="dropdown-item" href="Pre_paper.php"><span class="sr-only">(current)</span>Previous Paper</a>
+            <a class="dropdown-item" href="Scedule.php"><span class="sr-only">(current)</span>Schedule</a>
+          </div>
+        </li>
+          <a class="nav-item nav-link" href="#">Staff <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Infrastructure <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Library <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Alumni <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Placement <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" data-toggle="modal" data-target="#myModal">Log In<span class="sr-only">(current)</span></a>
 
-<?php sqlsrv_free_stmt($stmt);
-sqlsrv_close($conn);
-?>
-</body>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</html>
+            <!-- Modal --> <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+              <!-- Modal content-->
+              <div class="modal-content">
+                <!-- Modal Header -->
+                <div class="modal-header">
+                  <h4 class="modal-title">Login</h4>
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                  <nav class="form-group">
+                    <div class="container-fluid">
+                      <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                          <form id="Login">
+                            <br>
+                            <input type="email" name="Email" placeholder="User Id" required class="form-control"><br><br>
+                            <input type="password" name="Pss" placeholder="Password" required class="form-control"><br><br>
+                            <select name="Role" id="role" class="form-control">
+                                  <option value="" disabled selected hidden>Choose Role</option>
+                                  <option value="Admin">Admin</option>
+                                  <option value="HOD">HOD</option>
+                                  <option value="Faculty">Faculty</option>
+                                  <option value="TechStaff">Technical Staff</option>
+                            </select><br><br>
+                            <input type="submit" name="Login" value="Login" class="btn-primary">
+                          </form >
+                        </div>
+                      </div>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
