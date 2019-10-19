72c1cfdba7543fd025d1017eb0d67a88c57f3a1e
Nikhil Pandey <vinayak.pp73@gmail.com>
2019-10-19 23:58:40 +0530

    .

---
+++ Library.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="Login.css">
+</head>
+<body>
+<?php include 'header.php';?>
+
+<?php include 'connection.php' ;?>
+
+<?php
+ {
+  //echo "printing the table\n";
+  ?>
+  <div class="container">
+    <div class="row">
+
+
+      <div class="col-md-12">
+        <h4>Previous Paper</h4>
+        <div class="table-responsive">
+   <table id="mytable" class="table table-bordred table-striped">
+
+            <thead>
+              <th>Paper Name</th>
+              <th>Held Date</th>
+              <th>Download</th>
+            </thead>
+
+            <tbody>
+                <?php
+
+            }
+            ?>
+          </tbody>
+
+        </table>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
+</body>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</html>
---
+++ Scedule.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="Login.css">
+</head>
+<body>
+<?php include 'header.php';?>
+
+<?php include 'connection.php' ;?>
+
+<?php
+ {
+  //echo "printing the table\n";
+  ?>
+  <div class="container">
+    <div class="row">
+
+
+      <div class="col-md-12">
+        <h4>Schedule</h4>
+        <div class="table-responsive">
+   <table id="mytable" class="table table-bordred table-striped">
+            
+
+            <tbody>
+                <?php
+
+            }
+            ?>
+          </tbody>
+
+        </table>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
+</body>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</html>
