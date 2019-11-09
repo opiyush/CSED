def2bdc86ec961f8f89edf0307d924af02470f60
Nikhil Pandey <vinayak.pp73@gmail.com>
2019-11-09 18:54:48 +0530

    Sigup button added

--- header.php
+++ header.php
@@ -34,6 +34,7 @@
           <a class="nav-item nav-link" href="#">Alumni <span class="sr-only">(current)</span></a>
           <a class="nav-item nav-link" href="#">Placement <span class="sr-only">(current)</span></a>
           <a class="nav-item nav-link" data-toggle="modal" data-target="#myModal">Log In<span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" data-toggle="modal" data-target="#myModal2">Sign Up<span class="sr-only">(current)</span></a>
             <!-- Modal --> <div id="myModal" class="modal fade" role="dialog">
             <div class="modal-dialog">
               <!-- Modal content-->
@@ -68,6 +69,48 @@
                 </div>
               </div>
             </div>
+          </div>
+
+           <!-- Modal --> <div id="myModal2" class="modal fade" role="dialog">
+           <div class="modal-dialog">
+             <!-- Modal content-->
+             <div class="modal-content">
+               <!-- Modal Header -->
+               <div class="modal-header">
+                 <h4 class="modal-title">Sign Up</h4>
+                 <button type="button" class="close" data-dismiss="modal">&times;</button>
+               </div>
+               <div class="modal-body">
+                 <nav class="form-group">
+                   <div class="container-fluid">
+                     <div class="tab-content" id="nav-tabContent">
+                       <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                         <form id="Login" action="signup_backend.php" method="post">
+                           <br>
+                           <input type="text" name="EmpNo" placeholder="Employe No" required class="form-control"><br><br>
+                           <input type="text" name="Name" placeholder="Full Name" required class="form-control"><br><br>
+                           <input type="email" name="Email" placeholder="Email Id" required class="form-control"><br><br>
+                           <input type="text" name="UserId" placeholder="User Id" required class="form-control"><br><br>
+                           <input type="password" name="Password" placeholder="Password" required class="form-control"><br><br>
+                           <input type="text" name="Phn1" placeholder="Contact No." required class="form-control"><br><br>
+                           <input type="text" name="Phn2" placeholder="Alternate Contact No." class="form-control"><br><br>
+                           <input type="text" name="Degree" placeholder="Degree" required class="form-control"><br><br>
+                           <input type="text" name="CVlink" placeholder="CV Link"  class="form-control"><br><br>
+                           <input type="text" name="Photo" placeholder="Photo Link"  class="form-control"><br><br>
+                           <select name="Designation" id="role" class="form-control">
+                                 <option value="" disabled selected hidden>Choose Role</option>
+                                 <option value="2">Faculty</option>
+                                 <option value="3">Technical Staff</option>
+                           </select><br><br>
+                           <input type="submit" name="Sign Up" value="Sign Up" class="btn-primary">
+                         </form >
+                       </div>
+                     </div>
+                   </div>
+                 </nav>
+               </div>
+             </div>
+           </div>
           </div>
         </div>
       </div>
---
+++ signup_backend.php
@@ -0,0 +1,48 @@
+<?php include 'connection.php'?>
+
+<?php
+    $EmpNo = $_POST['EmpNo'];
+    $Name = $_POST['Name'];
+    $Email = $_POST['Email'];
+    $UserId = $_POST['UserId'];
+    $Password = $_POST['Password'];
+    $Phn1 = $_POST['Phn1'];
+    $Phn2 = $_POST['Phn2'];
+    $Degree = $_POST['Degree'];
+    $CVlink = $_POST['CVlink'];
+    $Photo = $_POST['Photo'];
+    $Designation = $_POST['Designation'];
+    $params = array($Email,$Password,$Designation);
+    $stmt = sqlsrv_query( $conn, "SELECT COUNT(Password) AS valid FROM emp_details WHERE Email=? and Password=? and Designation=?",$params);
+    if( sqlsrv_fetch( $stmt ) === false)
+    {
+     die( print_r( sqlsrv_errors(), true));
+    }
+
+   $valid = sqlsrv_get_field( $stmt, 0);
+   echo "$valid: ";
+  if($valid == 1)
+  {
+    $params = array($EmpNo,$Name,$UserId,$Phn1,$Phn2,$Degree,$CVlink,$Photo);
+    print_r($pararms);
+    $stmt = sqlsrv_query( $conn,"Insert into emp_details (EmpNo,Name, UserId, Phn1, Phn2, Degree, CVlink, Photo) VALUES (?,?,?,?,?,?,?,?);",$params);
+    echo $stmt;
+      if($Designation== 0)//0 for admin
+      {
+        header("Location: admin_page.php");
+      }
+      elseif ($Designation ==1) {// 1 for HOD
+        header('Location: admin_page.php');
+      }
+      elseif ($Designation ==2) {//2 for Faculty
+        echo 'the role is Faculty';
+      }
+
+      elseif ($Designation == 3) {//3 for Technical Staff
+        echo "TechStaff";
+      }
+  }
+  else {
+    echo '<h2>invalid User ID or Password</h2>';
+  }
+ ?>
