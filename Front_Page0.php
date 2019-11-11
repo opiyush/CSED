4273570cb4895ade08e1afc8b18911c3d8923d43
opiyush <45115329+opiyush@users.noreply.github.com>
2019-11-11 22:15:28 +0530

    Login update

---
+++ Front_Page0.php
@@ -0,0 +1,115 @@
+<?php
+session_start();
+   ?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Computer Science and Engineering</title>
+  <link rel="stylesheet" href="Front_Page_CSS.css">
+  <link rel="stylesheet" href="Login.css">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+<?php include 'header.php';?>
+  <!-- Carousel -->
+  <div id="carouselExampleFade" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-item active">
+        <img src="http://www.knit.ac.in/images/administration_img.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="http://www.knit.ac.in/images/bui_09.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+
+      </div>
+      <div class="carousel-item">
+        <img src="https://cdn.s3waas.gov.in/s38f53295a73878494e9bc8dd6c3c7104f/uploads/2018/03/2018031234.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+  <div class="container">
+    <div class="row">
+      <!--News Section Start-->
+      <div class="col-lg-6 col-md-6 news_header">
+        <h3>Announcements</h3>
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-xs-12">
+                <ul class="demo1">
+                  <li class="news-item">
+                    <a href="">some news or announcements 1 </a>
+                    <!--<span></span>-->
+                  </li>
+                  <li class="news-item">
+                    <a href="">some news or announcements 2 </a>
+                    <!--<span></span>-->
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="panel-footer"><a href="" class="btn btn-primary">All News</a></div>
+          <!--    <ul class="pagination pull-right" style="margin: 0px;">
+          <li><a href="#" class="prev"><span class="glyphicon glyphicon-chevron-down"></span></a>
+        </li>
+        <li><a href="#" class="next"><span class="glyphicon glyphicon-chevron-up"></span></a>
+      </li>
+    </ul> -->
+    <div class="clearfix"></div>
+  </div>
+</div>
+
+<!--News Section End-->
+<div class="welcome">
+  <h2>Welcome!</h2>
+  <h3>Our Vision</h3>
+  <p>here is the vision of cse department of knit Sultanpur.</p>
+  <h3>Our Mission</h3>
+  <p>here is the mission of cse department of knit Sultanpur.</p>
+</div>
+</div>
+<div class='space'>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<div class="footer" >
+  <a class="nav-item nav-link" href="#">Contact Us <span class="sr-only">(current)</span></a>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ login_backend0.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+ ?>
+<?php include 'connection.php'?>
+
+<?php
+    $email = $_POST['Email'];
+    $pass = $_POST['Pss'];
+    $role = $_POST['Role'];
+    $params = array($email,$pass,$role);
+    $stmt = sqlsrv_query( $conn, "SELECT COUNT(Password) AS valid FROM emp_details WHERE Email=? and Password=? and Designation=?",$params);
+    if( sqlsrv_fetch( $stmt ) === false)
+    {
+     die( print_r( sqlsrv_errors(), true));
+   }
+   $valid = sqlsrv_get_field( $stmt, 0);
+   //echo "$valid: ";
+  if($valid == 1)
+  {
+    $_SESSION["email"]=$email; //assigning email to session variable
+      if($role == 0)//0 for admin
+      {
+        $_SESSION["role"]='Admin'; //assigning role to session variable
+        header("Location: admin_page.php");
+      }
+      elseif ($role ==1) {// 1 for HOD
+        $_SESSION["role"]="HOD"; //assigning role to session variable
+        header('Location: admin_page.php');
+      }
+      elseif ($role ==2) {//2 for Faculty
+        $_SESSION["role"]="Faculty";//assigning role to session variable
+        header("Location: Faculty_login.php");
+      }
+      elseif ($role == 3) {//3 for Technical Staff
+        $_SESSION["role"]="TechStaff";//assigning role to session variable
+        echo 'The role is TechStaff';
+      }
+  }
+  else {
+    echo '<h2>invalid User ID or Password</h2>';
+  }
+
+ ?>
