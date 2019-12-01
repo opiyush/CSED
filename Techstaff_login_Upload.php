c43ce39a6d7181d6e951466472df039af9920cf5
Nikhil Pandey <vinayak.pp73@gmail.com>
2019-12-01 20:03:44 +0530

    previous uploads can be seen

--- Techstaff_login_Upload.php
+++ Techstaff_login_Upload.php
@@ -47,7 +47,64 @@ if(isset($_SESSION["role"])){
         </div>
       </form>
 
+
       </div>
+      <<?php
+      $stmt = sqlsrv_query( $conn, "select * from TechStaff_Upload where Uploaded_By=?",array($email)); //making query and storing it in stmt variable
+       ?>
+
+
+
+       <?php
+       if ($stmt !== NULL) {
+         //echo "printing the table\n";
+         ?>
+         <div class="container" style="padding:13%">
+           <div class="row">
+
+
+             <div class="col-md-12">
+               <h4>Previous Uploads</h4>
+               <div class="table-responsive">
+          <table id="mytable" class="table table-bordred table-striped">
+
+                   <thead>
+                     <th>Heading</th>
+                     <th>Publised Date</th>
+                     <th>Details</th>
+                   </thead>
+
+                   <tbody>
+                     <?php
+                     while($rows = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
+
+                       ?>
+
+
+                       <tr>
+                         <td><?php echo $rows["Heading"] ?></td>
+                         <td><?php $Date = $rows["Date"]->format('d/m/Y'); echo $Date;?></td>
+                         <td><?php $Data = $rows["Data"]; echo $Data; ?></td>
+                       </tr>
+                       <?php
+                     }
+                   }
+                   ?>
+                 </tbody>
+
+               </table>
+
+             </div>
+
+           </div>
+         </div>
+       </div>
+       <?php sqlsrv_free_stmt($stmt);
+       sqlsrv_close($conn);
+       ?>
+
+
+
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
