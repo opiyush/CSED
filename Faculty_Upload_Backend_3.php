976d4818dec98e2bfbb1926af249f4d1dd8a1f9e
Nikhil Pandey <vinayak.pp73@gmail.com>
2019-12-06 10:44:02 +0530

    missing file

---
+++ Faculty_Upload_Backend_3.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+  <?php include 'connection.php' ?>
+  <?php
+     $Heading = $_POST['Heading'];
+     $Data = $_POST['Data'];
+     $Date = date("d-m-Y");
+     $Uploaded_By = $_POST['Uploaded_By'];
+     //$subject = $_POST['subject'];
+     //$sql = 'INSERT INTO Table_1 (Name, EmpNo) VALUES (?, ?)';
+     $params = array($Heading,$Data,$Date,$Uploaded_By);
+
+     $stmt = sqlsrv_query( $conn,"Insert into Other_Upload (Heading, Data, Date, Uploaded_By) VALUES (?,?,?,?);",$params);
+     if($stmt!=NULL)
+     {
+       echo "true"
+       ?>
+
+       <form>
+       	<input type="button" id="return_back" value="Return to previous page" onClick="javascript:history.go(-1)" />
+       </form>
+  <?php
+     }
+     else{
+       ?>
+       <h1>
+         Error Occoured please try again
+        </h1>
+       <?php     }
+  ?>
+  <script type="text/javascript">
+  document.getElementById("return_back").click();
+  </script>
+
+</head>
+<body>
+  <h1></h1>
+</body>
+</html>
