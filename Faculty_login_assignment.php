6ba85747bfb0422693f40c315f2192ef61ba6bd7
Nikhil Pandey <vinayak.pp73@gmail.com>
2020-02-17 22:44:22 +0530

    partial

--- Faculty_login_assignment.php
+++ Faculty_login_assignment.php
@@ -19,7 +19,7 @@ if(isset($_SESSION["role"])){
     </head>
     <style media="screen">
       .container{
-        margin-left: 20%;
+        margin-left: 16%;
       }
       .row{
         margin-left: 3rem;
