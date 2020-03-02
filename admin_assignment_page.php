48812d07db3411b5b219156d1dfadafddf77b640
Nikhil Pandey <vinayak.pp73@gmail.com>
2020-03-02 18:00:16 +0530

    limited view

--- admin_assignment_page.php
+++ admin_assignment_page.php
@@ -52,12 +52,11 @@ if(isset($_SESSION["role"])){
           while($rows = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
           ?>
             <tr>
-
-            <td><?php echo $rows["Heading"] ?></td>
+            <td><?php echo substr($rows["Heading"], 0, 8).".." ?></td>
             <td><?php echo $rows["Published_date"]?></td>
             <td><?php echo $rows["Due_date"]?></td>
             <td><?php echo $rows["Emp_Id"]?></td>
-            <td><?php echo $rows["Assg_Link"]?></td>
+            <td><?php echo substr($rows["Assg_Link"],0,8).".."?></td>
             <td><?php echo $rows["Sub_Code"]?></td>
 
             <!-- delete and edit -->
