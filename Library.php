16667b769cce99dc732fe36b968614fe99151372
Nikhil Pandey <vinayak.pp73@gmail.com>
2020-03-03 18:58:35 +0530

    minor

--- Library.php
+++ Library.php
@@ -3,8 +3,8 @@
 <html lang="en" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="header.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="header.css">
 </head>
 <body>
 <?php include 'header.php';?>
